f9e6d672520dc2c0fce7f9ccd3a3b5589cf9cb63
dianazepeda3 <90267006+dianazepeda3@users.noreply.github.com>
2023-03-03 16:08:13 -0600

    Valores aleatorios
    Numeros random para basura y para aspiradora

--- Aspiradora parte 1/Form1.Designer.cs
+++ Aspiradora parte 1/Form1.Designer.cs
@@ -30,109 +30,134 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.picBox_Basura1 = new System.Windows.Forms.PictureBox();
+            this.picBox_Aspiradora1 = new System.Windows.Forms.PictureBox();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.picBox_Basura2 = new System.Windows.Forms.PictureBox();
+            this.picBox_Aspiradora2 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnValores = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCaract = new System.Windows.Forms.Label();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_Basura1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_Aspiradora1)).BeginInit();
             this.panel3.SuspendLayout();
             this.panel4.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_Basura2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_Aspiradora2)).BeginInit();
+            this.panel5.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.panel1);
             this.panel2.Location = new System.Drawing.Point(38, 77);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(447, 266);
             this.panel2.TabIndex = 1;
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(215, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "1";
+            // 
             // panel1
             // 
             this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
             this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel1.Controls.Add(this.pictureBox2);
-            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.picBox_Basura1);
+            this.panel1.Controls.Add(this.picBox_Aspiradora1);
             this.panel1.Location = new System.Drawing.Point(14, 14);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(420, 235);
             this.panel1.TabIndex = 1;
             // 
-            // pictureBox2
+            // picBox_Basura1
             // 
-            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(107, 159);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(59, 61);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.picBox_Basura1.BackColor = System.Drawing.Color.Transparent;
+            this.picBox_Basura1.Image = ((System.Drawing.Image)(resources.GetObject("picBox_Basura1.Image")));
+            this.picBox_Basura1.Location = new System.Drawing.Point(107, 159);
+            this.picBox_Basura1.Name = "picBox_Basura1";
+            this.picBox_Basura1.Size = new System.Drawing.Size(59, 61);
+            this.picBox_Basura1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox_Basura1.TabIndex = 1;
+            this.picBox_Basura1.TabStop = false;
             // 
-            // pictureBox1
+            // picBox_Aspiradora1
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(210, 118);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(101, 102);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.picBox_Aspiradora1.BackColor = System.Drawing.Color.Transparent;
+            this.picBox_Aspiradora1.Image = ((System.Drawing.Image)(resources.GetObject("picBox_Aspiradora1.Image")));
+            this.picBox_Aspiradora1.Location = new System.Drawing.Point(210, 118);
+            this.picBox_Aspiradora1.Name = "picBox_Aspiradora1";
+            this.picBox_Aspiradora1.Size = new System.Drawing.Size(101, 102);
+            this.picBox_Aspiradora1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox_Aspiradora1.TabIndex = 0;
+            this.picBox_Aspiradora1.TabStop = false;
             // 
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.label4);
             this.panel3.Controls.Add(this.panel4);
             this.panel3.Location = new System.Drawing.Point(523, 77);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(447, 266);
             this.panel3.TabIndex = 2;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(221, 252);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "2";
+            // 
             // panel4
             // 
             this.panel4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel4.BackgroundImage")));
             this.panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel4.Controls.Add(this.pictureBox3);
-            this.panel4.Controls.Add(this.pictureBox4);
+            this.panel4.Controls.Add(this.picBox_Basura2);
+            this.panel4.Controls.Add(this.picBox_Aspiradora2);
             this.panel4.Location = new System.Drawing.Point(14, 14);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(420, 235);
             this.panel4.TabIndex = 1;
             // 
-            // pictureBox3
+            // picBox_Basura2
             // 
-            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(106, 159);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(59, 61);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 1;
-            this.pictureBox3.TabStop = false;
+            this.picBox_Basura2.BackColor = System.Drawing.Color.Transparent;
+            this.picBox_Basura2.Image = ((System.Drawing.Image)(resources.GetObject("picBox_Basura2.Image")));
+            this.picBox_Basura2.Location = new System.Drawing.Point(106, 159);
+            this.picBox_Basura2.Name = "picBox_Basura2";
+            this.picBox_Basura2.Size = new System.Drawing.Size(59, 61);
+            this.picBox_Basura2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox_Basura2.TabIndex = 1;
+            this.picBox_Basura2.TabStop = false;
             // 
-            // pictureBox4
+            // picBox_Aspiradora2
             // 
-            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(210, 118);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(101, 102);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox4.TabIndex = 0;
-            this.pictureBox4.TabStop = false;
+            this.picBox_Aspiradora2.BackColor = System.Drawing.Color.Transparent;
+            this.picBox_Aspiradora2.Image = ((System.Drawing.Image)(resources.GetObject("picBox_Aspiradora2.Image")));
+            this.picBox_Aspiradora2.Location = new System.Drawing.Point(210, 118);
+            this.picBox_Aspiradora2.Name = "picBox_Aspiradora2";
+            this.picBox_Aspiradora2.Size = new System.Drawing.Size(101, 102);
+            this.picBox_Aspiradora2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox_Aspiradora2.TabIndex = 0;
+            this.picBox_Aspiradora2.TabStop = false;
             // 
             // label1
             // 
@@ -146,53 +171,74 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "Agente Reactivo Simple";
             // 
-            // button1
+            // btnValores
             // 
-            this.button1.BackColor = System.Drawing.Color.IndianRed;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(38, 365);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 46);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "INICIAR";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnValores.BackColor = System.Drawing.Color.IndianRed;
+            this.btnValores.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnValores.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnValores.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnValores.Location = new System.Drawing.Point(643, 378);
+            this.btnValores.Name = "btnValores";
+            this.btnValores.Size = new System.Drawing.Size(223, 46);
+            this.btnValores.TabIndex = 4;
+            this.btnValores.Text = "RESETEAR VALORES";
+            this.btnValores.UseVisualStyleBackColor = false;
+            this.btnValores.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // panel5
             // 
-            this.button2.BackColor = System.Drawing.Color.IndianRed;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button2.Location = new System.Drawing.Point(174, 365);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(107, 46);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "DETENER";
-            this.button2.UseVisualStyleBackColor = false;
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.panel5.Controls.Add(this.lblCaract);
+            this.panel5.Controls.Add(this.label2);
+            this.panel5.Location = new System.Drawing.Point(38, 365);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(447, 71);
+            this.panel5.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(135, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Caracteristicas del Entorno";
+            // 
+            // lblCaract
+            // 
+            this.lblCaract.Location = new System.Drawing.Point(63, 43);
+            this.lblCaract.Name = "lblCaract";
+            this.lblCaract.Size = new System.Drawing.Size(337, 16);
+            this.lblCaract.TabIndex = 2;
+            this.lblCaract.Text = "--";
+            this.lblCaract.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(1000, 441);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1000, 461);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.btnValores);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_Basura1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_Aspiradora1)).EndInit();
             this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.panel4.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_Basura2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_Aspiradora2)).EndInit();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -202,15 +248,19 @@
 
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox picBox_Basura1;
+        private System.Windows.Forms.PictureBox picBox_Aspiradora1;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox picBox_Basura2;
+        private System.Windows.Forms.PictureBox picBox_Aspiradora2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnValores;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblCaract;
     }
 }
 
--- Aspiradora parte 1/Form1.cs
+++ Aspiradora parte 1/Form1.cs
@@ -15,6 +15,72 @@ namespace Aspiradora_parte_1
         public Form1()
         {
             InitializeComponent();
+            reset_val();
         }
+
+        public void reset_val()
+        {
+            /* Basura
+             * -0 Vacio
+             * -1 Izquierda
+             * -2 Derecha
+             * -3 Ambas
+             * 
+             * Aspiradora
+             * -0 Izquierda
+             * -1 Derecha
+             */
+            int basura, aspiradora;
+            Random random = new Random();
+            basura = random.Next(0, 4);
+            aspiradora = random.Next(0, 2);
+
+            if (aspiradora == 0)
+            {
+                picBox_Aspiradora1.Visible = true;
+                picBox_Aspiradora2.Visible = false;
+                lblCaract.Text = "Aspiradora en habitación 1";
+
+            }
+            else
+            {
+                picBox_Aspiradora1.Visible = false;
+                picBox_Aspiradora2.Visible = true;
+                lblCaract.Text = "Aspiradora en habitación 2";
+            }
+
+            switch (basura)
+            {
+                case 0:
+                    picBox_Basura1.Visible = false;
+                    picBox_Basura2.Visible = false;
+                    lblCaract.Text = lblCaract.Text + "- Habitaciones Limpias";
+                    break;
+
+                case 1:
+                    picBox_Basura1.Visible = true;
+                    picBox_Basura2.Visible = false;
+                    lblCaract.Text = lblCaract.Text + " - Habitación 1 Sucia";
+                    break;
+
+                case 2:
+                    picBox_Basura1.Visible = false;
+                    picBox_Basura2.Visible = true;
+                    lblCaract.Text = lblCaract.Text + " - Habitación 2 Sucia";
+                    break;
+
+                case 3:
+                    picBox_Basura1.Visible = true;
+                    picBox_Basura2.Visible = true;
+                    lblCaract.Text = lblCaract.Text + " - Habitaciones Sucias";
+                    break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            reset_val();   
+        }
+        
     }
 }
